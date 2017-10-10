5e72031abca9894eddef3fa3db42cf9e73957895
Rutger07 <r.terhorst@students.uu.nl>
2017-10-10 12:54:09 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,74 +1,79 @@
-#assignment getting and cleaning data
-#goal prepare a tidy dataset (that can be used for later analysis)
+###############################################################################################
+############################ Assignment getting and cleaning data #############################
+###############################################################################################
+############## Goal: prepare a tidy dataset (that can be used for later analysis) #############
+###############################################################################################
+############################### submit 1 tidy dataset                 #########################
+############################### submit a link to a github repository  #########################
+############################### submit a code book called CodeBook.md #########################
+############################### also include a README.md in the repo  #########################
 
-#submit 1 tidy dataset
-#submit a link to a github repository
-#submit a code book called CodeBook.md
-#also include a README.md in the repo
+#load packages
+library(plyr)
 
-#1. merge test & trainingset
-
-#get names of all features
+#load datafiles, mapping tables, names, tidy features
+# get names of all features -> required for giving column names to all measurements in data
 features <- read.table("UCI HAR Dataset/features.txt")
-
-#get test data in dataframe
-  subjects_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-  x_test <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "")
-    #4. appropriate variable names
-    names(x_test) <- features[[2]]
-  y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activity")
-  
-  
-  test_data <- cbind(subjects_test, x_test, y_test, "test")
-  #4. appropriate variable names
-  names(test_data)[ncol(test_data)] <- "test_train"
-#get train data in dataframe
-subjects_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "")
-  names(x_train) <- features[[2]]
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activity")
-
-train_data <- cbind(subjects_train, x_train, y_train, "train")
-names(train_data)[ncol(train_data)] <- "test_train"
-
-#merge data from test and train
-complete_data <- rbind(test_data, train_data)
-
-#2. Extracts only the measurements on the mean and standard deviation for each measurement.
-#mean
-mean <- colMeans(complete_data[,2:562], na.rm = FALSE, dims = 1)
-
-#stdev
-numeric_data <- complete_data[,2:562]
-numeric_data <- as.matrix(numeric_data)
-stdev <- colSds(numeric_data)
-
-#3. descriptive activity names
-#load mapping table
+# descriptive activity names
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+names(activity_labels) <- c("activity", "activity_description")
 
-#give names to columns
-names(activity_labels) <- c("activity", "description")
+###############################################################################################
+########################## merge test & trainingset (Task #1)##################################
+###############################################################################################
 
-#merge
-mergedDF <- merge(complete_data, activity_labels, key = activity)
+############## get test data in dataframe -> processing of test data to merge #################
+subjects_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject") #subjects 
+measurements_test <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "")                #measurements belonging to subjects
+names(measurements_test) <- features[[2]]                                                   #column names
+activity_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activity")      #activity belonging to measurements
+test_data <- cbind(subjects_test, activity_test, measurements_test)                         #combine test data
 
-#sub column 1 with newly acquired column 'descriptive'
-mergedDF$activity <- mergedDF[,565]
-mergedDF[,565] <- NULL
+############# get train data in dataframe -> processing of train data to merge ################
+subjects_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+measurements_train <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "")
+names(measurements_train) <- features[[2]]
+activity_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+train_data <- cbind(subjects_train, activity_train, measurements_train)
 
-# tidy data set is mergedDF with descriptive column values for activities 
-# and all column names for different measurements
+############################# merge data from test and train #################################
+complete_dataset <- rbind(test_data, train_data)
 
-#split DF in 2 seperate dataframes to calculate means per subject and measurement (max is 478 measurements - has 561)
-DF1 <- mergedDF[,1:300]
-DF2 <- mergedDF[,c(1,2,301:563)]
-m1 <- aggregate(.~subject + activity, data = DF1, FUN = function(x) mean(x))
-#check dimensions
-dim(m1)
-m2 <- aggregate(.~subject + activity, data = DF2, FUN = function(x) mean(x))
-#check dimensions
-dim(m2)
-#bind the two seperate Df summaries together
-tidy_df <- cbind(m1,m2[,3:265])
+###############################################################################################
+###### Extract only measurements on the mean and std dev for each measurement (Task #2) #######
+###############################################################################################
 
+dataset_mean_stdev <- complete_dataset[,grepl("mean|std|subject|activity", names(complete_dataset))]
+
+###############################################################################################
+########################### Descriptive activity names (Task #3) ##############################
+###############################################################################################
+
+descriptive_dataset_mean_stdev <- join(dataset_mean_stdev, activity_labels, by = "activity", match = "first")
+descriptive_dataset_mean_stdev$activity <- NULL
+
+###############################################################################################
+############################## Use Appropriate Labels (Task #4) ###############################
+###############################################################################################
+
+names(descriptive_dataset_mean_stdev) <- sub("^t", "Time.Domain_", names(descriptive_dataset_mean_stdev))
+names(descriptive_dataset_mean_stdev) <- sub("Freq", "Frequency", names(descriptive_dataset_mean_stdev))
+names(descriptive_dataset_mean_stdev) <- sub("^f", "Frequency.Domain_", names(descriptive_dataset_mean_stdev))
+names(descriptive_dataset_mean_stdev) <- sub("Acc", "Acceleration", names(descriptive_dataset_mean_stdev))
+names(descriptive_dataset_mean_stdev) <- sub("std", "Std_Dev", names(descriptive_dataset_mean_stdev))
+names(descriptive_dataset_mean_stdev) <- sub("Mag", "Magnitude", names(descriptive_dataset_mean_stdev))
+names(descriptive_dataset_mean_stdev) <- sub("mean", "Mean", names(descriptive_dataset_mean_stdev))
+
+###############################################################################################
+################ Independent Tidy Data Set Mean of Activity & Person (Task #5) ################
+###############################################################################################
+
+
+tidy_dataset_mean <- 
+  aggregate(.~subject + activity_description, 
+            data = descriptive_dataset_mean_stdev, 
+            FUN = function(x) mean(x))
+
+
+#write to txt file
+write.table(tidy_dataset_mean, "Independent_Tidy_Dataset.txt", row.names = FALSE)
